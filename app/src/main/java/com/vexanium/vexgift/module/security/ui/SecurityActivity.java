cb2e614fd6e6ae8177c8bcc8b85e5abe89e0c91e
Hizkia Prana Lemuel <hizkia.p.l@gmail.com>
2018-07-21 15:20:27 +0700

    extra commit

--- app/src/main/java/com/vexanium/vexgift/module/security/ui/SecurityActivity.java
+++ app/src/main/java/com/vexanium/vexgift/module/security/ui/SecurityActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 
 import com.vexanium.vexgift.R;
 import com.vexanium.vexgift.annotation.ActivityFragmentInject;
+import com.vexanium.vexgift.base.BaseActivity;
 
 @ActivityFragmentInject(contentViewId = R.layout.activity_security, toolbarTitle = R.string.security_toolbar_title)
-public class SecurityActivity extends AppCompatActivity {
+public class SecurityActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView() {
+
     }
 }
